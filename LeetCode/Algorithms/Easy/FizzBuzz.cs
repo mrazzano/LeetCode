39f65992650c588697c94d6ddb35bf614699b5c1
mrazzano <mrazzano@comcast.net>
2017-05-03 10:32:11 -0700

    Added Exercises

---
+++ LeetCode/Algorithms/Easy/FizzBuzz.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using LeetCode.Library;
+
+namespace LeetCode.Algorithms.Easy
+{
+    public class FizBuzz : IQuestion
+    {
+        private const string question = "Fizz Buzz";
+
+        public void Run(int order)
+        {
+            Utility.PrintQuestionHeader(order, question);
+
+            const int n = 15;
+            Utility.PrintArray(solution(n).ToArray());
+        }
+
+        private static List<string> solution(int n)
+        {
+            var result = new List<string>();
+            for (var i = 1; i <= n; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    result.Add("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    result.Add("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    result.Add("Buzz");
+                }
+                else
+                {
+                    result.Add(i.ToString());
+                }
+            }
+            return result;
+        }
+    }
+}
